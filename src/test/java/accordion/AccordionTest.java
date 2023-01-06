94bb8b7abb34c188d63d6082426b7534471a0419
Vad-kot <vadim-dog123@yandex.ru>
2023-01-06 13:34:24 +0300

    Исправлено название директории

--- src/test/java/accordion/AccordionTest.java
+++ src/test/java/accordion/AccordionTest.java
@@ -1,4 +1,4 @@
-package mainPage.accordion;
+package accordion;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
--- src/test/java/order/PositiveScenarioOrderingScooterTest.java
+++ src/test/java/order/PositiveScenarioOrderingScooterTest.java
@@ -1,4 +1,4 @@
-package mainPage.positiveScenarioOrderingScooter;
+package order;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
